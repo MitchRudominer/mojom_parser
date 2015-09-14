e35a311f7054c5813c53b1d540fcd80a50940e5d
Alexandre Zani <azani@chromium.org>
2015-09-14 15:43:09 -0700

    Rename TestTokenChan to TestTokenize.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -102,7 +102,7 @@ func TestSkipSkippable(t *testing.T) {
 	checkEq(t, DOT, tokens[0].Kind)
 }
 
-func TestTokenChan(t *testing.T) {
+func TestTokenize(t *testing.T) {
 	ts := Tokenize("   \t .   ")
 	token := ts.PeekNext()
 	checkEq(t, DOT, token.Kind)
